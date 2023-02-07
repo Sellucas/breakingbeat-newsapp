8f1627234076b95ff90e9ba1713361f41643a2ce
Sellucas <lucassellmachado@gmail.com>
2023-02-07 15:15:07 -0300

    Navbar Footer components

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full bg-red-700 bottom-0 fixed text-white">
+      <div className="w-[1200px] m-auto h-28 pt-2">
+        <div><a href="/" className="logo text-3xl">BB</a> últimas notícias</div>
+        <div className="flex justify-between mt-8 text-sm">
+          <div>© Copyright 2023-2026 Sellucas LTDA.</div>
+          <div>política de privacidade  |  princípios editoriais  |  anuncie conosco</div>
+        </div>
+      </div>
+      </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineSearch } from "react-icons/ai";
+import "../../index.css"
+
+const Navbar = () => {
+  return (
+    <div className="w-full bg-red-700 top-0">
+      <div className="w-[1200px] h-16 m-auto flex direction-row justify-between font-bold items-center text-white">
+        <div className="flex">
+          <GiHamburgerMenu className="mr-2" />
+          <div className="flex direction-row items-center text-sm">MENU</div>
+        </div>
+        <div className="logo text-3xl"><a href="/">BB</a></div>
+        <div className="flex">
+          <AiOutlineSearch className="mr-2" />
+          <div className="flex direction-row items-center text-sm">BUSCAR</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
