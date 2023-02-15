87beeb654a8d4a9ab0e1be4af46a721659203371
Sellucas <lucassellmachado@gmail.com>
2023-02-15 15:46:28 -0300

    Economy API get data

--- src/components/home/economy/Economy.jsx
+++ src/components/home/economy/Economy.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Economy = () => {
+  const [economy, setEconomy] = useState(null);
+
+  const fetchEconomy = async () => {
+    return axios
+      .get("https://economia.awesomeapi.com.br/last/USD-BRL%2CEUR-BRL")
+      .then((response) => {
+        setEconomy(null);
+        setEconomy(response.data);
+      });
+  };
+
+  useEffect(() => {
+    fetchEconomy();
+  }, []);
+
   return (
     <div className="bg-white border-2 h-96 w-full grid place-items-center">
-      <div>Economy</div>
+      <div>
+        <div>
+          <p>Dólar {economy?.USDBRL.bid}</p>
+          <p>Euro {economy?.EURBRL.bid}</p>
+        </div>
+      </div>
     </div>
   );
 };
